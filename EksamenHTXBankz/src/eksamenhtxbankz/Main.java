e285c1e9c735d9f1e2c61d7bb08f13f1168a7d0c
VictorKPetersen <83222801+VictorKPetersen@users.noreply.github.com>
2022-03-28 10:37:59 +0200

    Classes
    Main Classes

--- EksamenHTXBankz/src/eksamenhtxbankz/Main.java
+++ EksamenHTXBankz/src/eksamenhtxbankz/Main.java
@@ -5,7 +5,11 @@
 package eksamenhtxbankz;
 
 
-public class MainWindow {
+public class Main {
 
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
