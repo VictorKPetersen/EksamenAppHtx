34a8c87d5143fd4c1c66294673eb4b018aebd3d4
strikZie <victor123456bp@gmail.com>
2022-04-04 10:38:02 +0200

    Update MainWindow.java

--- EksamenHTXBankz/src/eksamenhtxbankz/MainWindow.java
+++ EksamenHTXBankz/src/eksamenhtxbankz/MainWindow.java
@@ -4,15 +4,24 @@
 */
 package eksamenhtxbankz;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 public class MainWindow extends JFrame{
-
+    JPanel homePanel;
+    JPanel kontoPanel;
+    JPanel udgiftPanel;
+    JPanel indkomstPanel;
+    JPanel opsparPanel;
+    
+    
+    
     private JButton homeKontoBtn;
     private JButton homeUdgiftBtn;
-    private JButton homePlanUdgiftBtn;
     private JButton homeIndkomstBtn;
     private JButton homeOpsparBtn;
     
@@ -21,6 +30,62 @@ public class MainWindow extends JFrame{
     }
     
     private void CreateComponents(){
+        homePanel = new JPanel();
+        kontoPanel = new JPanel();
+        udgiftPanel = new JPanel();
+        indkomstPanel = new JPanel();
+        opsparPanel = new JPanel();
+        
+        
+        homeKontoBtn = new JButton("Konto");
+        homeUdgiftBtn = new JButton("Udgifter");
+        homeIndkomstBtn = new JButton("Indkomst");
+        homeOpsparBtn = new JButton("Hjælp til opspar");
+        
+        homeKontoBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switchPanels(homePanel, kontoPanel);
+            }
+        });
+        
+        homeUdgiftBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switchPanels(homePanel, udgiftPanel);
+            }
+        });
+        
+        homeIndkomstBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switchPanels(homePanel, indkomstPanel);
+            }
+        });
+        
+        homeOpsparBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switchPanels(homePanel, opsparPanel);
+            }
+        });
+        
+        homePanel.add(homeKontoBtn);
+        homePanel.add(homeUdgiftBtn);
+        homePanel.add(homeIndkomstBtn);
+        homePanel.add(homeOpsparBtn);
+        
+        kontoPanel.add(homeUdgiftBtn);
+        
+        add(homePanel);
+        
+        
+    }
+    
+    public void switchPanels(JPanel currentPanel, JPanel newPanel){
+        add(newPanel);
+        remove(currentPanel);
+        //add(newPanel);
         
     }
 }
